334e7af86d281eb4b51e7d4ec936314165335e45
Mansi Wakharkar <mansiswakharkar@gmail.com>
2024-12-18 21:10:43 +0530

    Python fundamentals - Day 1
    #### **Day 1: Getting Started**
    - Install Python and Jupyter Notebook (or set up an IDE like PyCharm).
    - Learn the basics: variables, data types, input/output.
    - Practice: Write a program to accept user input and display it.

---
+++ PythonFundamentals.py
@@ -0,0 +1,14 @@
+
+#Write a program to accept user input and display it.
+num = int(input('Enter a number'))
+print('Your input: ', num)
+
+
+str1, str2 = input('Enter strings: ').split()
+print('Input Strings: ', str1, str2)
+
+
+
+
+
+
