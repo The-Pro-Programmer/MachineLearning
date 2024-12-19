ffddac809592af3ece09ac3cdd6facf47068bf67
Mansi Wakharkar <mansiswakharkar@gmail.com>
2024-12-19 23:50:41 +0530

    Day 2: Lists
    Day 2: Lists**
    - Understand lists: creation, indexing, slicing.
    - Common operations: append, extend, insert, remove, sort, reverse.
    - Practice: Create a list of numbers and perform the above operations.

--- PythonFundamentals.py
+++ PythonFundamentals.py
@@ -1,13 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
 
 #Write a program to accept user input and display it.
 num = int(input('Enter a number'))
 print('Your input: ', num)
 
 
+# In[6]:
+
+
 str1, str2 = input('Enter strings: ').split()
 print('Input Strings: ', str1, str2)
 
 
+# In[2]:
+
+
+arr = [10, 11, 12, 13, 14]
+
+
+# In[3]:
+
+
+print('First Element: ', arr[0])
+print('Last Eelement: ', arr[-1])
+
+
+# In[4]:
+
+
+sublist1 = [item for item in arr if item >=12]
+print('Original list: ', arr)
+print('Sublist: ', sublist1)
+
+
+# In[ ]:
 
 
 
